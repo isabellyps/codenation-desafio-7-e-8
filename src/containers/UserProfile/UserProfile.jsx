49b9526736ba9516e265888b8ebd1868ca86f601
Isabelly <isabelly.paganini@gmail.com>
2020-05-30 16:49:02 -0300

    created container userprofile

--- src/containers/UserProfile/UserProfile.jsx
+++ src/containers/UserProfile/UserProfile.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './UserProfile.scss';
+import "./UserProfile.scss";
 
 const UserProfile = ({ avatar, name, username }) => {
   return (
-    <section className="profile">
+    <section className="profile" data-testid="user-profile">
+      <div className="container">
+        <div className="profile-data">
+          <div className="user">
+            <div className="user__thumb">
+              <img
+                src="https://viniciusvinna.netlify.app/assets/api-instagram/profiles/profile-placeholder.png"
+                alt=""
+              />
+            </div>
+            <p className="user__name">
+              Nome
+              <span>@username</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 };
 
 export default UserProfile;
