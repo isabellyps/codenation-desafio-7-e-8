e40335936a7a1d7e01bbb277a181e72fb6d613d0
Isabelly <isabelly.paganini@gmail.com>
2020-05-30 16:46:12 -0300

    created container stories

--- src/containers/Stories/Stories.jsx
+++ src/containers/Stories/Stories.jsx
@@ -1,20 +1,43 @@
 import React, { useState } from "react";
 
-import Story from '../../components/Story';
+import Story from "../../components/Story";
 
-import './Stories.scss';
+import "./Stories.scss";
 
 const Stories = ({ stories, getUserHandler }) => {
+  const [showStory] = useState(false);
   return (
     <React.Fragment>
-      <section className="stories">
+      <section className="stories" data-testid="stories">
         <div className="container">
+          <button className={"user__thumb user__thumb--hasNew"}>
+            <div className="user__thumb__wrapper">
+              <img
+                src="https://viniciusvinna.netlify.app/assets//api-instagram/profiles/black-panther/black-panther-profile.jpg"
+                alt="T'Challa"
+              />
+            </div>
+          </button>
+          <button className="user__thumb false">
+            <div className="user__thumb__wrapper">
+              <img
+                src="https://viniciusvinna.netlify.app/assets//api-instagram/profiles/bruce/bruce-profile.jpg"
+                alt="Bruce Wayne"
+              />
+            </div>
+          </button>
+          <button className="user__thumb false">
+            <div className="user__thumb__wrapper">
+              <img
+                src="https://viniciusvinna.netlify.app/assets//api-instagram/profiles/gamora/gamora-profile.jpg"
+                alt="Gamora"
+              />
+            </div>
+          </button>
         </div>
       </section>
 
-      {showStory && (
-        <Story />
-        )}
+      {showStory && <Story />}
     </React.Fragment>
   );
 };
